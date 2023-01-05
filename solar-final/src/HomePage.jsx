3f9822bbbaf39785ddc42b882f5218fb0937aa6d
James Cogan <james.m.cogan@gmail.com>
2023-01-05 17:42:20 -0500

    m

---
+++ solar-final/src/HomePage.jsx
@@ -0,0 +1,40 @@
+import SettingsBar from "./SettingsBar";
+import BodyBar from "./BodyBar";
+import LeftBodyInfoCard from "./LeftBodyInfoCard"
+import RightBodyInfoCard from "./RightBodyInfoCard"
+import { useState, useEffect } from "react";
+
+function HomePage({setPlanetType, 
+                   planetType, 
+                   clickedBody, 
+                   setClickedBody, 
+                   setClickedbodyObject, 
+                   clickedBodyObject,}) {
+
+const [leftObject, setLeftObject] = useState()
+const [rightObject, setRightObject] = useState()
+const [tripLocation, setTripLocation] = useState('start')
+
+  useEffect(()=>{
+    tripLocation === "end" ? setRightObject(clickedBodyObject) : setLeftObject(clickedBodyObject)
+  },[clickedBodyObject])
+
+    return(
+        <div>
+          <header>
+            <h3>Space Travel Simulator</h3>
+                <SettingsBar setPlanetType={setPlanetType} setTripLocation={setTripLocation}/>
+                    <BodyBar 
+                        planetType={planetType} 
+                        clickedBody={clickedBody} 
+                        setClickedBody={setClickedBody} 
+                        setClickedbodyObject={setClickedbodyObject}/>
+            </header>
+          <div className="main-display">
+        <LeftBodyInfoCard clickedBodyObject={leftObject} classname={"left-main-display"}/>
+        <RightBodyInfoCard clickedBodyObject={rightObject} classname={"right-main-display"}/>
+      </div>
+     </div>
+    )
+}
+export default HomePage
---
+++ solar-final/src/RightBodyInfoCard.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react"
+
+
+//builds a card that displays the info of the ody that was clicked
+export default function RightBodyInfoCard({clickedBodyObject, className}) {
+
+    //states to hold the text info of the body
+    const [bodyDistanceFromSun, setBodyDistanceFromSun] = useState("")
+    const [yearLenght, setYearLength]  = useState('')
+    const [dayLength, setDayLength] =useState('')
+
+    //sets the distance of the body
+    function displayDistance(body){
+        if(body === undefined){return}
+        if (body.name==="Sun"){setBodyDistanceFromSun("You cannot get closer to the Sun")}
+        else{
+            const formatedDistance = body.distanceFromSun.toLocaleString()
+            setBodyDistanceFromSun(formatedDistance + " km from the Sun")
+        }
+    }
+    useEffect(() => {
+        displayDistance(clickedBodyObject)
+    },[clickedBodyObject])
+
+    //sets the length of a year of the body
+    function displayYearLength(body){
+        if(body === undefined){return}
+        if (body.name==="Sun"){setYearLength("It is complicated -- Look up Sun barycenter")}
+        else{setYearLength(body.lengthOfYear + "to go around the sun")}
+    }
+    useEffect(() => {
+        displayYearLength(clickedBodyObject)
+    },[clickedBodyObject])
+
+    //sets the length of a day of the body
+    function displayDayLength(body){
+        if(body === undefined){return}
+        else{setDayLength("A day is " + body.lengthOfDay)}
+    }
+    useEffect(() => {
+        displayDayLength(clickedBodyObject)
+    },[clickedBodyObject])
+
+
+    //we check to see if the object is undefined - it would be on startup - so we dont try to pull info from nothing 
+    if (clickedBodyObject === undefined){
+        return (
+            <div>Pick a planet</div>
+        )
+    } else{
+        return(     
+            <div className="left-main-display">
+                <div className="planet">
+                <h1 className="location-text">End Location</h1>
+                    <img className="main-planet-detail-image" alt="planetImage" src={clickedBodyObject.image}></img>
+                </div>
+                <div className="info">
+                    <h1 className="main-planet-name">{clickedBodyObject.name}</h1>
+                    <div className="facts">
+                        <h2 className="distance-from-sun">{bodyDistanceFromSun}</h2>
+                        <h2 className="year-length">{yearLenght}</h2>
+                        <h2 className="day-length">{dayLength}</h2>
+                    </div>
+                </div>
+            </div>            
+        )
+    }
+}
