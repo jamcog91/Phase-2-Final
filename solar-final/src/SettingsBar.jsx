9606dde0d502075aa76cc9f586cc36dedc3a96df
Avinash Ramsumair <avi@pure235games.com>
2023-01-05 05:03:38 -0500

    main display 02

--- solar-final/src/SettingsBar.jsx
+++ solar-final/src/SettingsBar.jsx
@@ -4,23 +4,24 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import { useState, useEffect } from 'react';
 
-
+//is in charge of the settings for the program such as which bodies to display
 export default function SettingsBar({ setPlanetType}) {
 
+    //state to hold the value of the drop down bar
     const [dropDownValue, setDropdownValue] = useState('');
-
+    //function to update the dropdown state when we change the drop down 
     const bodyTypeDropDownChange = (event) => {
         setDropdownValue(event.target.value);
     };
-    
+    //we use useEffect to stop an infinit loop 
     useEffect(() => {
         setPlanetType(dropDownValue)        
     }, [dropDownValue])
 
     
-
     return(
         <div className='settings-bar'>
+            {/* the drop down bar */}
             <FormControl sx={{ m: 1, minWidth: 160 }} size="small" variant="filled">
                 <InputLabel>Body Type</InputLabel>
                 <Select
