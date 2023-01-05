2516aca5736fd505ea23418c8ee8c3306f5d0fec
Avinash Ramsumair <avi@pure235games.com>
2023-01-04 22:43:19 -0500

    planet Bar 05

--- solar-final/src/BodyBar.jsx
+++ solar-final/src/BodyBar.jsx
@@ -6,6 +6,7 @@ export default function BodyBar({planetType}){
     const [dwarfPlanetsArray, setDwarfPlanetsArray] = useState([])
     const [moonsArray, setMoonsArray] = useState([])
 
+    
     const fetchPlanets = async () => {
         const req = await fetch('http://localhost:3001/planets')
         const res = await req.json()
@@ -14,7 +15,7 @@ export default function BodyBar({planetType}){
     useEffect(() => {
         fetchPlanets()
     }, [])
-
+    
     const fetchDwarfPlanets = async () => {
         const req = await fetch('http://localhost:3001/dwarfPlanets')
         const res = await req.json()
@@ -23,7 +24,7 @@ export default function BodyBar({planetType}){
     useEffect(() => {
         fetchDwarfPlanets()
     },[])
-
+    
     const fetchMoons = async () => {
         const req = await fetch('http://localhost:3001/moons')
         const res = await req.json()
@@ -32,8 +33,21 @@ export default function BodyBar({planetType}){
     useEffect(() => {
         fetchMoons()
     },[])
-
     
+    
+    const [clickedBody, setClickedBody] = useState("")
+    const [planetsMoonArray, setPlanetsMoonArray] = useState([])
+
+    useEffect(() => {
+        createPlanetArray()
+    }, [clickedBody])
+
+    function createPlanetArray(){
+        const fillteredMoonArray = moonsArray.filter((moon) => {
+            return clickedBody === moon.parent
+        })
+        setPlanetsMoonArray(fillteredMoonArray)
+    }
 
     function populateBodyList(array){
         array.map((body) => {
@@ -69,10 +83,11 @@ export default function BodyBar({planetType}){
   }
 
 
-  function BodyCard ({body}){
+
+  function BodyCard ({body, setClickedBody}){
 
     const [mouseOverImage, setMouseOverImage] = useState(1)
-    const [mouseOverText, setMouseOverText] = useState(0)
+    const [mouseOverText, setMouseOverText] = useState(0)   
 
     return(
         <div 
@@ -98,6 +113,7 @@ export default function BodyBar({planetType}){
                 src={body.image} 
                 alt={body.name} 
                 style={{opacity: mouseOverImage}}
+                
             />
         </div>
     )
