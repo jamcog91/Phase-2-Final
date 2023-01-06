1e2b5bbd5fc0d81343a9af585a636d67e1c0a658
Avi <112576586+aviramsumair@users.noreply.github.com>
2023-01-06 10:58:07 -0500

    Update PlanetProfile.jsx

--- solar-final/src/PlanetProfile.jsx
+++ solar-final/src/PlanetProfile.jsx
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-function PlanetProfile({planetType}) {
-    return(
-        <div></div>
-=======
-function PlanetProfile({clickedBodyObject}) {
-    console.log(clickedBodyObject)
-    return(
-        <div className="PlanetProfile">
-            <div className="Planet-container">
-            <img src={clickedBodyObject.image} alt={clickedBodyObject.name} />
-            </div>
-        </div>
->>>>>>> james
-    )
-}
-export default PlanetProfile
+
